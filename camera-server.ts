b97f4ec7e7b79707eb24d94c2716758152d25d3d
rexcklee <rexcklee852@gmail.com>
2024-08-15 10:29:34 -0500

    update to use mysql database

--- camera-server.ts
+++ camera-server.ts
@@ -8,7 +8,7 @@ import cors from 'cors';
 import categoryRoutes from './routes/category-routes'; // Import the routes
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Path to SSL certificates
 // const certPath = "/etc/letsencrypt/live/rexlee.space/";
